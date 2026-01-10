259a1477c0f00f1d1f9e3f1957c3c40496764032
hy4ri <hiari45@gmail.com>
2026-01-10 17:12:45 +0300

    proprietaryCodecs

--- flake.nix
+++ flake.nix
@@ -16,16 +16,32 @@
 
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      });
   in {
-    packages = forAllSystems (system: {
-      vivaldi-snapshot = nixpkgsFor.${system}.callPackage ./package.nix {};
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      base = pkgs.callPackage ./package.nix {};
+    in {
+      vivaldi-snapshot = base;
+      # Version with proprietary codecs for video playback
+      vivaldi-snapshot-with-codecs = base.override {
+        proprietaryCodecs = true;
+        vivaldi-ffmpeg-codecs = pkgs.vivaldi-ffmpeg-codecs;
+      };
       default = self.packages.${system}.vivaldi-snapshot;
     });
 
     # Overlay for easy integration into NixOS configurations
     overlays.default = final: prev: {
       vivaldi-snapshot = final.callPackage ./package.nix {};
+      vivaldi-snapshot-with-codecs = (final.callPackage ./package.nix {}).override {
+        proprietaryCodecs = true;
+        vivaldi-ffmpeg-codecs = final.vivaldi-ffmpeg-codecs;
+      };
     };
   };
 }
