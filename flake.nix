46d879df3893673ad0f1ff546d3ae9010b217a63
Hy4ri <hiari45@gmail.com>
2026-06-29 18:36:46 +0300

    Update vivaldi-ffmpeg-codecs to fix symbol lookup error

--- flake.nix
+++ flake.nix
@@ -21,11 +21,47 @@
         inherit system;
         config.allowUnfree = true;
       });
+
+    customFFmpegCodecs = pkgs: let
+      system = pkgs.stdenv.hostPlatform.system;
+      systemInfo = {
+        "x86_64-linux" = {
+          revision = "117";
+          hash = "1yzv1jav8cq5rz76ifhf2w7z3vh7zaf66abq8g160b0dbyh3nhb0";
+          folder = "chromium-ffmpeg-git-2026-05-18";
+        };
+        "aarch64-linux" = {
+          revision = "116";
+          hash = "16ifx9jj3fc9an5n5znixszhdyf2icj7mc81rir596jy1wwra6g1";
+          folder = "chromium-ffmpeg-git-2026-03-16";
+        };
+      }.${system} or (throw "Unsupported system for custom codecs: ${system}");
+    in
+      pkgs.stdenv.mkDerivation {
+        pname = "chromium-codecs-ffmpeg-extra";
+        version = systemInfo.folder;
+
+        src = pkgs.fetchurl {
+          url = "https://api.snapcraft.io/api/v1/snaps/download/XXzVIXswXKHqlUATPqGCj2w2l7BxosS8_${systemInfo.revision}.snap";
+          sha256 = systemInfo.hash;
+        };
+
+        nativeBuildInputs = [ pkgs.squashfsTools ];
+
+        unpackPhase = ''
+          unsquashfs -dest . $src
+        '';
+
+        installPhase = ''
+          install -vD ${systemInfo.folder}/chromium-ffmpeg/libffmpeg.so $out/lib/libffmpeg.so
+        '';
+      };
   in {
     packages = forAllSystems (system: let
       pkgs = nixpkgsFor.${system};
+      codecs = customFFmpegCodecs pkgs;
       commonArgs = {
-        vivaldi-ffmpeg-codecs = pkgs.vivaldi-ffmpeg-codecs;
+        vivaldi-ffmpeg-codecs = codecs;
         enableWidevine = true;
         widevine-cdm = pkgs.widevine-cdm;
       };
@@ -37,7 +73,7 @@
 
     overlays.default = final: _prev: {
       vivaldi-snapshot = final.callPackage ./vivaldi-snapshot.nix {
-        vivaldi-ffmpeg-codecs = final.vivaldi-ffmpeg-codecs;
+        vivaldi-ffmpeg-codecs = customFFmpegCodecs final;
         enableWidevine = true;
         widevine-cdm = final.widevine-cdm;
       };
@@ -45,7 +81,7 @@
 
     overlays.stable = final: _prev: {
       vivaldi-stable = final.callPackage ./vivaldi-stable.nix {
-        vivaldi-ffmpeg-codecs = final.vivaldi-ffmpeg-codecs;
+        vivaldi-ffmpeg-codecs = customFFmpegCodecs final;
         enableWidevine = true;
         widevine-cdm = final.widevine-cdm;
       };
